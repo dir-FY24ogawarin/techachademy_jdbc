c88ae1f269f6de45f9ac4755079d7caa8098b6ed
dir-FY24ogawarin <rin.ogawa.h226@dirbato.co.jp>
2024-04-15 16:00:45 +0900

    Lesson9終わり

---
+++ techacademy_jdbc/src/dbSample/TryWithResourcesSample01.java
@@ -0,0 +1,52 @@
+package dbSample;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class TryWithResourcesSample01 {
+
+    public static void main(String[] args) {
+        
+  
+        try {
+            //1.ドライバーのクラスをJava上で読み込む
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            
+            //4.5.Select文の実行と結果を格納/代入
+            String sql = "SELECT * FROM country LIMIT 50";
+           
+
+            // try-with-resources構文
+            try (// DBと接続する
+              Connection con = DriverManager.getConnection(
+                  "jdbc:mysql://localhost/world?useSSL=false&allowPublicKeyRetrieval=true", "root",
+                  "@Orin280925");
+              // DBとやりとりする窓口（Statementオブジェクト）の作成
+              Statement stmt = con.createStatement();
+              // SQLを発行
+              ResultSet rs = stmt.executeQuery(sql);) {
+
+              // 結果を表示する
+              while (rs.next()) {
+                // Name列の値を取得
+                String name = rs.getString("Name");
+                // 取得した値を表示
+                System.out.println(name);
+              }
+            }
+
+          } catch (ClassNotFoundException e) {
+            System.err.println("JDBCドライバのロードに失敗しました。");
+            e.printStackTrace();
+          } catch (SQLException e) {
+            System.err.println("データベースに異常が発生しました。");
+            e.printStackTrace();
+          }
+        }
+
+      }
